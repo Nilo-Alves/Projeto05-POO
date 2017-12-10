b7a61de8770129c8dafb6076382a821575390047
Gabriela Duarte <gabrielasantosduarte@yahoo.com.br>
2017-12-10 15:03:09 -0200

    Criação da Classe aluno e página aluno(precisa arrumar a página aluno).

---
+++ src/java/com/notas/web/Aluno.java
@@ -0,0 +1,26 @@
+package com.notas.web;
+
+import java.util.ArrayList;
+
+public class Aluno {
+    private String nome;
+    private String matricula;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+   
+    
+}
