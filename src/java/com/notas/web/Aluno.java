29755b8667bb721946467b73f059d1668d142357
Karin <>
2017-12-10 19:36:50 -0200

    Criado o método getAluno (Falta BD para conexão)

--- src/java/com/notas/web/Aluno.java
+++ src/java/com/notas/web/Aluno.java
@@ -1,10 +1,42 @@
 package com.notas.web;
 
+import java.sql.SQLException;
+
 
 public class Aluno {
     private String nome;
     private String matricula;
-
+    private String login;
+    private String senha;
+    
+     public static Aluno getAluno(String login, String password) throws SQLException{
+     /*  
+         ***Modificar de acordo com o Banco de Dados***
+         
+         
+         String SQL = "SELECT * FROM users WHERE login=? AND pass_hash=?";
+        PreparedStatement s = Database.getConnection().prepareStatement(SQL);
+        s.setString(1, login);
+        s.setString(2, password.hashCode()+"");
+        ResultSet rs = s.executeQuery();
+        */Aluno a = null;
+        /*if(rs.next()){
+            p = new Professor(rs.getInt("id")
+                    , rs.getString("name")
+                    , rs.getString("login")
+                    , rs.getString("pass_hash"));
+        }
+        rs.close();
+        s.close();*/
+        return a; 
+    }
+     public Aluno(String nome, String matricula, String login, String senha) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.login = login;
+        this.senha = senha;
+    }
+     
     public String getNome() {
         return nome;
     }
@@ -20,6 +52,24 @@ public class Aluno {
     public void setMatricula(String matricula) {
         this.matricula = matricula;
     }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    
    
     
 }
